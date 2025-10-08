1d256d8b57d413554b59dfb58df12691c272bcca
PacchaDavid <david.paccha@unl.edu.ec>
2025-10-08 15:59:20 -0500

    development mode

--- fit-mentor/src/auth/auth.module.ts
+++ fit-mentor/src/auth/auth.module.ts
@@ -19,10 +19,10 @@ import { AuthGuard } from './auth.guard';
   ],
   providers: [
     AuthService,
-    {
+    /* {
       provide: APP_GUARD,
       useClass: AuthGuard
-    },
+    }, */
   ],
   controllers: [AuthController],
   exports: [AuthService]
